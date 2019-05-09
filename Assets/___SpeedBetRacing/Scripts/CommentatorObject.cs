18f5f476be7165392f028bd18439bb55b614714f
Clement <clement.roth@live.fr>
2019-05-09 10:33:05 +0200

    .

--- Assets/___SpeedBetRacing/Scripts/CommentatorObject.cs
+++ Assets/___SpeedBetRacing/Scripts/CommentatorObject.cs
@@ -13,35 +13,35 @@ public class CommentatorObject : ScriptableObject
         public List<AudioClip> startRace;
 
         public List<AudioClip> firstRankDreamShark;
-        public List<AudioClip> secondeRankDreamShark;
+        public List<AudioClip> secondRankDreamShark;
         public List<AudioClip> thirdRankDreamShark;
         public List<AudioClip> fourthRankDreamShark;
         public List<AudioClip> fifthRankDreamShark;
         public List<AudioClip> explosionDreamShark;
         
         public List<AudioClip> firstRankTropicalBear;
-        public List<AudioClip> secondeRankTropicalBear;
+        public List<AudioClip> secondRankTropicalBear;
         public List<AudioClip> thirdRankTropicalBear;
         public List<AudioClip> fourthRankTropicalBear;
         public List<AudioClip> fifthRankTropicalBear;
         public List<AudioClip> explosionTropicalBear;
         
         public List<AudioClip> firstRankOrangeEarth;
-        public List<AudioClip> secondeRankOrangeEarth;
+        public List<AudioClip> secondRankOrangeEarth;
         public List<AudioClip> thirdRankOrangeEarth;
         public List<AudioClip> fourthRankOrangeEarth;
         public List<AudioClip> fifthRankOrangeEarth;
         public List<AudioClip> explosionOrangeEarth;
 
         public List<AudioClip> firstRankSerenity;
-        public List<AudioClip> secondeRankSerenity;
+        public List<AudioClip> secondRankSerenity;
         public List<AudioClip> thirdRankSerenity;
         public List<AudioClip> fourthRankSerenity;
         public List<AudioClip> fifthRankSerenity;
         public List<AudioClip> explosionSerenity;
         
         public List<AudioClip> firstRankBurningLine;
-        public List<AudioClip> secondeRankBurningLine;
+        public List<AudioClip> secondRankBurningLine;
         public List<AudioClip> thirdRankBurningLine;
         public List<AudioClip> fourthRankBurningLine;
         public List<AudioClip> fifthRankBurningLine;
